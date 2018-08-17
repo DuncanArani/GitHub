48c9e46731d29d357ecd7f9d7a09f6c4f1804f90
ARUNCO <duncanarani@gmail.com>
2018-08-17 19:06:17 +0300

    api

--- src/app/prof/profile.services.ts
+++ src/app/prof/profile.services.ts
@@ -2,4 +2,8 @@ import { Injectable } from '@angular/core';
 
 
 @Injectable()
-export class ProfileServices {}
+export class ProfileServices {
+    private query: string;
+    private API_KEYS: string;
+    private API_URL: string;
+}
