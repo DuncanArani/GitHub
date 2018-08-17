31d8cb958d4715217fc9e1fb6af20d4aaf7ed962
ARUNCO <duncanarani@gmail.com>
2018-08-17 17:13:07 +0300

    m

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileDetailsComponent } from './profile-details/profile-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    ProfileDetailsComponent,
   ],
+
   imports: [
     BrowserModule
   ],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,12 @@
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  GitHub_API_KEYS: '8a8a3edc5eda71ff70a96ba38a2ea69972d3d47c ',
+GitHub_URL_KEYS: 'https://github.com/settings/tokens'
 };
 
 /*
