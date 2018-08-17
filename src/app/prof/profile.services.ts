4a34c7af2144316daccfee997bbbbbdd936d69b1
ARUNCO <duncanarani@gmail.com>
2018-08-17 19:16:20 +0300

    API

--- src/app/prof/profile.services.ts
+++ src/app/prof/profile.services.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 
 @Injectable()
 export class ProfileServices {
     private query: string;
-    private API_KEYS: string;
-    private API_URL: string;
+    private API_KEYS: string = environment.GitHub_API_KEYS;
+    private API_URL: string = environment.GitHub_API_KEYS;
 }
